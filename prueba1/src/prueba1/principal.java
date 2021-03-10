b9fab797dd482a9578f2b4f8ef8aa535ac7d517f
jromerov12 <jromerov12@iesalbarregas.es>
2021-03-10 18:31:22 +0100

    validamos primer cmmit

---
+++ prueba1/src/prueba1/principal.java
@@ -0,0 +1,9 @@
+package prueba1;
+public class principal {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+System.out.println("Hola mundo java-git");
+System.out.println("Hola mundo java-git Versión 2");
+System.out.println("Hola mundo. Pruebas Git. Se añade Cambio1");
+}
+}
